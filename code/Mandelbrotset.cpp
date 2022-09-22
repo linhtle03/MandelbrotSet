4dc00ded70c20802c3616581fc9e1070b1dae487
admin <admin@BCS-Ubuntu-40.lrccd.ad.losrios.edu>
2022-09-21 21:10:48 -0700

    changed name

---
+++ code/Mandelbrotset.cpp
@@ -0,0 +1,16 @@
+// Include important C++ libraries here
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+// Make code easier to type with "using namespace"
+using namespace sf;
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
+
+
