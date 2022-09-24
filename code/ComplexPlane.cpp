7b3351f04fa279078693a31952facfdeec6072f6
admin <admin@BCS-Ubuntu-09.lrccd.ad.losrios.edu>
2022-09-23 17:03:57 -0700

    nothing changed

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -53,27 +53,32 @@ void ComplexPlane::setMouseLocation(Vector2f coord)
 	m_mouseLocation.y = coord.y;
 }
 
-//void ComplexPlane::loadText(Text& text)
-//{
-	/*
-	Use a stringstream and the corresponding member variables to create the following output:
-		Mandelbrot Set
-		Center: (0,0)
-		Cursor: (cursor posx, cursor posy)
-		Left-click to Zoom in
-		Right-click to Zoom out
-	*/
-//}
+/*
+void ComplexPlane::loadText(Text& text)
+{
+	//Use a stringstream and the corresponding member variables to create the following output:
+    cout << "Mandelbrot Set" << endl;
+    cout << "Center: (" << VALUEX << "," << VALUEY << ")" << endl;
+    cout << "Cursor: (" << cursor posx << "," << cursor posy << ")" << endl;
+    cout << "Left-click to Zoom in" << endl;
+    cout << "Right-click to Zoom out" << endl;
 
-//size_t ComplexPlane::countIterations(Vector2f coord)
-//{
+} 
+*/
+
+/*
+size_t ComplexPlane::countIterations(Vector2f coord)
+{
 	// Count the number of iterations of the set for the given coordinate as specified above
 
-//}
+}
+*/
 
-//void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
-//{
+/*
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+{
 	// Map the given iteration count to an r,g,b color and assign the given reference variables
 
 	// You may want to start with gray scale, where r,g,b are always the same value in the range [0,255]
-//}
+}
+*/
